40275f14da73a71ae5421f665cc21e2fdb042158
joaokienen <42853031+joaokienen@users.noreply.github.com>
2021-10-04 13:42:48 -0300

    Create MailSender.sh

---
+++ MailSender/MailSender.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+###################################################################################################################################
+#                                                                                                                                 #
+# Script para validar e realizar envio de e-mails                                                                                 #
+#                                                                                                                                 #
+# Data de criação: 31/08/2021 | João Kienen                                                                                       #
+#                                                                                                                                 #
+#                                                                                                                                 #
+###################################################################################################################################
+
+DATE1=$(date +'%Y-%m-%d')
+EMAIL_FOR="email@domain"
+EMAIL_TO="email@domaindomain"
+MESSAGE=$(cat /recursos/Recursos__"$DATE1"__*)
+SERVER="smtp.domain:587"
+PASS="174H38gJsCec63BCA3zh94a1"
+
+sendemail -f $EMAIL_FOR -t $EMAIL_TO -u "Relatório de Recursos" -m "$MESSAGE" -s $SERVER -xu email@domain -xp $PASS -o message-charset="utf-8"
