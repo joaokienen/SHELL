13c794af97fb04dffc11cb4c90772685e7feb5e4
joaokienen <42853031+joaokienen@users.noreply.github.com>
2021-10-04 15:59:29 -0300

    Update and rename ServiceStatus/servicestatus.sh to STATUS/servicestatus.sh

--- STATUS/servicestatus.sh
+++ STATUS/servicestatus.sh
@@ -4,13 +4,12 @@
 #
 # Script de monitoramento de serviço!
 #
-# Autor: João Victor Kienen
+# Autor: João Kienen
 # Data de criação: 12/02/2021
 #
 # Descrição: Script de monitoramento de estado de serviço com o zabbix!
+# Description: Service state monitoring script with zabbix!
 # Validação com arquivo zabbix: /etc/zabbix/zabbix_agentd.conf
-#
-# Exemplo de uso: ./monitor.sh
 #
 ####################################################################################
 
