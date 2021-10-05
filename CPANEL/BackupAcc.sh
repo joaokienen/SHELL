417a377de4d38bd96bad9cd03359e838102e26d3
JvkScript <42853031+joaokienen@users.noreply.github.com>
2021-10-05 08:59:47 -0300

    Update BackupAcc.sh

--- CPANEL/BackupAcc.sh
+++ CPANEL/BackupAcc.sh
@@ -13,5 +13,5 @@ for i in `find /var/cpanel/suspended/ -mtime +30 | cut -d'/' -f5 | sed '1 d'`
     do
         /scripts/pkgacct $i;
         #sleep 10
-        #scp -P 22 /home/cpmove-"$i".tar.gz user@server:/etc/suspendedBackupcPanel/
+        #scp -p 2442 /home/cpmove-"$i".tar.gz user@server:/etc/suspendedBackupcPanel/
     done;
